a0d0c8a75298733be1a20808c415c634006aad0d
ma.xl <ma.xl@sand.com.cn>
2017-01-05 15:34:42 +0800

    commit

--- src/main/java/com/maxlong/study/ApplicationMain.java
+++ src/main/java/com/maxlong/study/ApplicationMain.java
@@ -1,9 +1,18 @@
 package com.maxlong.study;
 
+import org.jasig.cas.client.validation.Cas20ProxyReceivingTicketValidationFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 /**
  * @author 作者 maxlong:
@@ -13,13 +22,14 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 @SpringBootApplication
 public class ApplicationMain extends SpringBootServletInitializer {
 
+    public static final Logger logger = LoggerFactory.getLogger(ApplicationMain.class);
+
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(ApplicationMain.class);
     }
 
     public static void main(String[] args) throws Exception {
-        SpringApplication app = new SpringApplication(ApplicationMain.class);
-        app.run(args);
+        SpringApplication.run(ApplicationMain.class, args);
     }
 }
